8806fd2b2e2028581225f679da5c79ce78cd3feb
Gebreyesus <beteab@live.com>
2018-09-25 13:10:06 -0500

    added lines to .java file

--- src/Gebru_wordCounter.java
+++ src/Gebru_wordCounter.java
@@ -1,23 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gebru.project1;
 
 /**
- *
- * @author Beteab Gebru
- */
-public class Gebru_wordCounter {
+* @ Author: Beteab G. Gebru
+* Class: Compilers I
+* Project title: Word analysis on a word file
+*/
 
+public class Gebru_wordCounter 
+{
     /**
-     * @param args the command line arguments
+     * @param fileName- the command line argument provided by user(the name of the file to be analysed
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-       
-    }
-    
+    public static void main(String[] fileName) 
+	{
+        	File file = new File("sample.txt");
+		try(Scanner sc = new Scanner(new FileInputStream(file)))
+		{
+    			int count=0;
+    			while(sc.hasNext())
+			{
+        			sc.next();
+        			count++;
+			}
+	 	   System.out.println("Number of words: " + count);
+       		}
+    	}   
 }
