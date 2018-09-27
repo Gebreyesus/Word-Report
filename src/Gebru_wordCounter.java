8813fbd81cee1d9804eca03f81b9002f837724d3
Gebreyesus <beteab@live.com>
2018-09-26 23:07:19 -0500

    some additions to main functions

--- src/Gebru_wordCounter.java
+++ src/Gebru_wordCounter.java
@@ -6,35 +6,28 @@ import java.io.*;
 * Class: Compilers I
 * Project title: Word analysis on a word file
 */
-
 public class Words 
 {
-    
     public static void main(String[] args) throws FileNotFoundException 
     {
         HashMap wordMap = new HashMap();  // Map of word --> # of occurrences
 
         //scanner object is instantiated with name Cursor
         Scanner Cursor = new Scanner(new File("Sample.txt")); //file is opened to be read
-        
-        //keep reading until cursor has reached end of document
-        while (Cursor.hasNext()) 
-        {
-            String Word = Cursor.next();
 
-            if(wordMap.containsKey(Word)) 
+        while (Cursor.hasNext()) //keep reading until cursor has reached end of document
+        {
+            String Word = Cursor.next();//capturing the next word in text
+            if(wordMap.containsKey(Word)) //if this is another instance of the word we have encountered
              {
                 // if word instance exists --> increment the number of appearances +1
                 Integer count = (Integer)wordMap.get(Word);
                 wordMap.put(Word, new Integer(count.intValue() + 1));
              } 
-            else 
-             {
-                // we haven't seen this word, so add it with count of 1
+            else//New word at last -> add to the Map and make occurence=1  
                 wordMap.put(Word, new Integer(1));
-             }
+            
         }
-
         // now print out every word in the book, along with its count,
         // in alphabetical order
         ArrayList arraylist = new ArrayList(wordMap.keySet());
@@ -46,22 +39,16 @@ public class Words
           Integer count = (Integer)wordMap.get(key);
           System.out.println(key + " --> " + count);
         }
-
     }
-     
-     
-     
+
     static void wordAnalyser(File file)
     {
 
     }
-    
+
     static void buildWordMap(String anotherWord)
     {
 
-    }    
-	
- 
-  
+    }     
 }
 
